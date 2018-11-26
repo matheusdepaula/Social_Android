70f287a413e4fad653c0fb39c4bfe87511020291
Matheus De Paula <matheusdepaula.bsb@gmail.com>
2018-11-26 21:52:50 -0200

    Mudancas no layout

--- app/src/main/java/br.com.iesb.social/activities/ScreenSlidePagerActivity.kt
+++ app/src/main/java/br.com.iesb.social/activities/ScreenSlidePagerActivity.kt
@@ -5,11 +5,15 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentActivity
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
+import android.support.v4.content.ContextCompat
 import android.support.v4.view.ViewPager
 import br.com.iesb.social.R
 import br.com.iesb.social.animations.DepthPageTransformer
 import br.com.iesb.social.fragments.ScreenSlidePageFragment
 import br.com.iesb.social.animations.ZoomOutPageTransformer
+import kotlinx.android.synthetic.main.activity_main.*
+import android.view.View
+import android.support.v4.view.ViewPager.OnPageChangeListener
 
 /**
  * Number of pages to show
@@ -31,7 +35,17 @@ class ScreenSlidePagerActivity: FragmentActivity() {
         val pagerAdapter = ScreenSlidePagerAdapter(supportFragmentManager)
         mPager.adapter = pagerAdapter
 
-        mPager.setPageTransformer(true, DepthPageTransformer())
+        mPager.addOnPageChangeListener(object : OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {}
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+
+            override fun onPageSelected(position: Int) {
+                onPageChange(position)
+            }
+        })
+
+//        mPager.setPageTransformer(true, DepthPageTransformer())
     }
 
     override fun onBackPressed() {
@@ -44,6 +58,51 @@ class ScreenSlidePagerActivity: FragmentActivity() {
         }
     }
 
+    fun onPageChange(position: Int) {
+        restorePageItems()
+
+        var mIndicator = getIndicatorByPosition(position)
+        val context = this@ScreenSlidePagerActivity
+
+        val mDrawable = ContextCompat.getDrawable(context, R.drawable.checked_circle)
+
+        mDrawable?.let {
+            mIndicator.setBackground(it)
+        }
+    }
+
+    fun getIndicatorByPosition(position: Int): View {
+
+        return when(position) {
+            0 -> indicator1
+            1 -> indicator2
+            2 -> indicator3
+            3 -> indicator4
+            else -> indicator1
+        }
+    }
+
+    fun restorePageItems() {
+        val mDrawable = ContextCompat.getDrawable(this, R.drawable.circle)
+
+        mDrawable?.let {
+            indicator1.setBackground(it)
+            indicator2.setBackground(it)
+            indicator3.setBackground(it)
+            indicator4.setBackground(it)
+        }
+    }
+
+    fun getTitleByPosition(position: Int): String {
+        return when(position){
+            0 -> "Primeira"
+            1 -> "Segunda"
+            2 -> "Terceira"
+            3 -> "Quarta"
+            else -> "Outra"
+        }
+    }
+
     /**
      * A simple pager adapter that represents 4 ScreenSlidePageFragment objects, in
      * sequence.
@@ -51,6 +110,9 @@ class ScreenSlidePagerActivity: FragmentActivity() {
     private inner class ScreenSlidePagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
         override fun getCount(): Int = NUMBER_OF_PAGES
 
-        override fun getItem(position: Int): Fragment = ScreenSlidePageFragment()
+        override fun getItem(position: Int): Fragment {
+            val page = ScreenSlidePageFragment.instance(position, getTitleByPosition(position), "LoremInopu soihfa djfnc!")
+            return page
+        }
     }
 }
--- app/src/main/java/br.com.iesb.social/fragments/ScreenSlidePageFragment.kt
+++ app/src/main/java/br.com.iesb.social/fragments/ScreenSlidePageFragment.kt
@@ -6,9 +6,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.iesb_fragment.view.*
 
 class ScreenSlidePageFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?):
-            View? = inflater!!.inflate(R.layout.iesb_fragment, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val view = inflater!!.inflate(R.layout.iesb_fragment, container, false)
+
+        view.screen_title.setText(arguments?.getString("title") ?: "")
+        view.screen_sub_title.setText(arguments?.getString("subTitle") ?: "")
+
+        val position = arguments?.getInt("position")
+        position.let {
+            if (it == 3) {
+                view.enterButton.visibility = View.VISIBLE
+            }
+        }
+
+        return view
+    }
+
+    companion object {
+        fun instance(position: Int, title: String, subTitle: String): ScreenSlidePageFragment {
+            val fragment = ScreenSlidePageFragment()
+            val args = Bundle()
+            args.putInt("position", position)
+            args.putString("title", title)
+            args.putString("subTitle", subTitle)
+            fragment.setArguments(args)
+
+            return fragment
+        }
+    }
 }
